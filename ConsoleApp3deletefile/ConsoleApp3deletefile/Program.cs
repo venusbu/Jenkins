bcada00a4cfe78b59d4766db2d2e1a2a09f82889
venusbu <60775853+venusbu@users.noreply.github.com>
2020-07-08 14:33:36 +0530

    Add files via upload

---
+++ ConsoleApp3deletefile/ConsoleApp3deletefile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3deletefile
+{
+    public class Program
+    {
+            static void Main(string[] args)
+            {
+                Console.WriteLine(createMessage());
+            }
+            public static string createMessage()
+            {
+                return "Hello World";
+            }
+        }
+    }
